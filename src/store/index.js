0cedd8b24b95eadfe731dd84f41d246bde2d63ff
attoiumani <attoiumani0313@gamil.com>
2020-02-13 16:35:32 +0900

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,47 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from "axios";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    dataSetName: String,
+    dataSetTemp: Number,
+    dataSetMaxTemp: Number,
+    dataSetMinTemp: Number,
   },
   mutations: {
+    mutateDataSetName(state, payload) {
+      state.dataSetName = payload;
+    },
+    mutateDataSetTemp(state, payload) {
+      state.dataSetTemp = payload;
+    },
+    mutateDataSetMaxTemp(state, payload) {
+      state.dataSetMaxTemp = payload;
+    },
+    mutateDataSetMinTemp(state, payload) {
+      state.dataSetMinTemp = payload;
+    }
   },
   actions: {
+    commitDataSet(store) {
+    return axios.get(
+      "https://api.openweathermap.org/data/2.5/weather?q=Tokyo,jp&units=metric&appid=4dff50a83aa2145ba555d8f59e9d3ef0"
+    )
+    .then(response => {
+      store.commit('mutateDataSetName', response.data.name)
+      store.commit('mutateDataSetTemp', response.data.main.temp)
+      store.commit('mutateDataSetMaxTemp', response.data.main.temp_max)
+      store.commit('mutateDataSetMinTemp', response.data.main.temp_min)
+    })
+   }
   },
-  modules: {
-  }
+  getters: {
+    getStateDataSetName: (state) => state.dataSetName,
+    getStateDataSetTemp: (state) => state.dataSetTemp,
+    getStateDataSetMaxTemp: (state) => state.dataSetMaxTemp,
+    getStateDataSetMinTemp: (state) => state.dataSetMinTemp,
+   }
 })
