7c95ce97f8954c036b499ad75dfd63cd8eb096d8
attoiumani <attoiumani0313@gamil.com>
2020-02-14 11:15:27 +0900

    Create Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-card height="150">
+    <v-footer absolute class="font-weight-medium" color="indigo">
+      <v-col class="text-center" cols="12">
+        {{new Date().getFullYear()}} — <strong>Vuetify</strong>
+      </v-col>
+    </v-footer>
+  </v-card>
+</template>
