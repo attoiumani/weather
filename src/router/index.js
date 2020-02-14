fbfca4416724cbd50a4a16cfcd42815c558db66f
attoiumani <attoiumani0313@gamil.com>
2020-02-14 16:36:29 +0900

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,37 +1,37 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import logo from '../views/logo.vue'
-import wether from '../views/wether.vue'
-import home from '../views/home.vue'
-import About from '../views/About.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import logo from "../views/logo.vue";
+import wether from "../views/wether.vue";
+import home from "../views/home.vue";
+import About from "../views/About.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: home,
+    path: "/",
+    name: "home",
+    component: home
   },
   {
-    path: '/wether',
-    name: 'wether',
+    path: "/wether",
+    name: "wether",
     component: wether
   },
   {
-    path: '/logo',
-    name: 'logo',
+    path: "/logo",
+    name: "logo",
     component: logo
   },
   {
-    path: '/About',
-    name: 'About',
+    path: "/About",
+    name: "About",
     component: About
-   }
-  ]
+  }
+];
 
 const router = new VueRouter({
   routes
-})
+});
 
-export default router
+export default router;
