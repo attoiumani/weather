9cd3ebe820f660d9dc414f1a50517d58ef6e0ede
attoiumani <attoiumani0313@gamil.com>
2020-02-14 14:55:04 +0900

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,22 @@
 <template>
   <v-app>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list>
+        <v-list-tile
+          v-for="item in Items"
+          :key="item.title"
+          :to="item.path">
+          <v-list-tile-action>
+            <v-icon>{{item.icon}}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+
     <v-app-bar color="primary" dark app collapse-on-scroll>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="drawer=!drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>Title</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn icon>
@@ -16,3 +31,21 @@
     </v-app-bar>
   </v-app>
 </template>
+
+
+<script>
+export default {
+  name: "App",
+  data(){
+    return {
+      drawer: null,
+      Items: [
+          { title: 'Home', path: '/', icon: 'mdi-stack-overflow' },
+          { title: 'wether', path: '/wether', icon: 'mdi-vuetify' },
+          { title: 'logo', path: '/logo', icon: 'mdi-github-face' },
+          { title: 'About', path: '/About', icon: 'mdi-pac-man' },
+     ]
+    }
+  },
+};
+</script>
