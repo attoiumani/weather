af8089c40ad4783989a4c32b99c9d5069c402e15
attoiumani <attoiumani0313@gamil.com>
2020-02-14 15:21:23 +0900

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,14 +2,11 @@
   <v-app>
     <v-navigation-drawer v-model="drawer" app>
       <v-list>
-        <v-list-tile
-          v-for="item in Items"
-          :key="item.title"
-          :to="item.path">
+        <v-list-tile v-for="item in Items" :key="item.title" :to="item.path">
           <v-list-tile-action>
             <v-icon>{{item.icon}}</v-icon>
           </v-list-tile-action>
-          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
+          <v-list-tile-content>{{item.title}}</v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
