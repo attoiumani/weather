f8614e59b721330ef5180e7afcb296a3bb69c27d
attoiumani <attoiumani0313@gamil.com>
2020-02-14 11:05:14 +0900

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,16 @@
 <template>
-  <v-app id="inspire" blue>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-  </div>
-  </v-app>
+    <v-toolbar color=indigo>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>Title</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-toolbar>
 </template>
