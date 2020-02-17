cdcf528a502da9fff2495b424f7bb8958b622f97
attoiumani <attoiumani0313@gamil.com>
2020-02-17 15:21:19 +0900

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,30 @@
 <template>
   <v-app>
-    <v-navigation-drawer v-model="drawer" clipped app>
-      <v-list>
-        <v-list-tile v-for="item in items" :key="item.title" :to="item.path">
-          <v-list-tile-action>
+    <v-navigation-drawer v-model="drawer" expand-on-hover clipped app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" :to="item.path">
+          <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
       </v-list>
     </v-navigation-drawer>
+
+
     <v-app-bar color="primary" dark clipped-left app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>Title</v-toolbar-title>
@@ -28,7 +41,8 @@ export default {
       items: [
         { title: "Home", path: "/", icon: "mdi-stack-overflow" },
         { title: "wether", path: "/wether", icon: "mdi-vuetify" },
-        { title: "logo", path: "/logo", icon: "mdi-github-face" }
+        { title: "logo", path: "/logo", icon: "mdi-github-face" },
+        { title: "About", path: "/About", icon: "mdi-github-face" },
       ]
     };
   }
