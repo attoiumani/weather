8ffdd862b0c3b08f3cccf54e198e733ab6cf74de
attoiumani <attoiumani0313@gamil.com>
2020-02-19 13:17:25 +0900

    Create TokyoW.vue

---
+++ src/components/Weather/TokyoW.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-app>
+    <v-card class="mx-auto" max-width="400">
+      <v-img class="white--text align-end" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg">
+        <v-card-title>{{dataOutputName}}</v-card-title>
+      </v-img>
+      <v-card-subtitle class="pb-0 text--primary">
+        Now
+        <div v-show="dataOutputLoading">
+          <v-progress-circular indeterminate color="primary"></v-progress-circular>
+        </div>
+        <div v-show="!dataOutputLoading">{{dataOutputTemp}}℃</div>
+      </v-card-subtitle>
+      <v-card-text>
+        <div>Today's high temperature {{dataOutputMaxTemp}}℃</div>
+        <div>Lowest Temperature {{dataOutputMinTemp}}℃</div>
+        <div>The current weather {{dataOutputCondition}}</div>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="orange" text>Share</v-btn>
+        <v-btn color="orange" text>Explore</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-app>
+</template>
+
+
+<script>
+export default {
+  computed: {
+    dataOutputName() {
+      return this.$store.getters.getStateDataSetName;
+    },
+    dataOutputTemp() {
+      return this.$store.getters.getStateDataSetTemp;
+    },
+    dataOutputMaxTemp() {
+      return this.$store.getters.getStateDataSetMaxTemp;
+    },
+    dataOutputMinTemp() {
+      return this.$store.getters.getStateDataSetMinTemp;
+    },
+    dataOutputCondition() {
+      return this.$store.getters.getStateDataSetCondition;
+    },
+    dataOutputLoading() {
+      return this.$store.getters.getStateDataSetLoading;
+    }
+  },
+  created: function() {
+    this.$store.dispatch("commitDataSet","Tokyo");
+  }
+  };
+</script>
