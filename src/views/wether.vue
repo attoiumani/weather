9b01b1d9499e5eee5e40d721f26800c9a6b5a8d1
attoiumani <attoiumani0313@gamil.com>
2020-02-19 14:28:56 +0900

    Update wether.vue

--- src/views/wether.vue
+++ src/views/wether.vue
@@ -1,16 +1,16 @@
 <template>
   <v-app>
     <v-row>
-      <v-col cols="12" sm="6" md="3" lg="6" xl="12">
+      <v-col cols="12" sm="12" md="6" lg="6" xl="12">
         <Tokyo/>
       </v-col>
-      <v-col cols="12" sm="6" md="3" lg="6" xl="12">
+      <v-col cols="12" sm="12" md="6" lg="6" xl="12">
         <Osaka/>
       </v-col>
-      <v-col cols="12" sm="6" md="3" lg="6" xl="12">
+      <v-col cols="12" sm="12" md="6" lg="6" xl="12">
         <Fukuoka/>
       </v-col>
-      <v-col cols="12" sm="6" md="3" lg="6" xl="12">
+      <v-col cols="12" sm="12" md="6" lg="6" xl="12">
         <Sapporo/>
       </v-col>
     </v-row>
