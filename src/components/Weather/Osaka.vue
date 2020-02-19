18fc7128385ec8e41f749763c487f3fd6e405dca
attoiumani <attoiumani0313@gamil.com>
2020-02-19 14:24:42 +0900

    Update Osaka.vue

--- src/components/Weather/Osaka.vue
+++ src/components/Weather/Osaka.vue
@@ -1,5 +1,4 @@
 <template>
-  <v-app>
     <v-card class="mx-auto" max-width="400">
       <v-img class="white--text align-end" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg">
         <v-card-title>{{city}}</v-card-title>
@@ -14,14 +13,13 @@
       <v-card-text>
         <div>Today's high temperature {{maxtemp}}℃</div>
         <div>Lowest Temperature {{mintemp}}℃</div>
-        <div>The current weather {{main}}</div>
+        <div>The current weather {{condition.main }}</div>
       </v-card-text>
       <v-card-actions>
         <v-btn color="orange" text>Share</v-btn>
         <v-btn color="orange" text>Explore</v-btn>
       </v-card-actions>
     </v-card>
-  </v-app>
 </template>
 
 
