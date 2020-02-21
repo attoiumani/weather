c70836199bc35c86ffc723607776fd814fb37b86
attoiumani <attoiumani0313@gamil.com>
2020-02-21 13:09:15 +0900

    Update Tokyo.vue

--- src/components/Weather/Tokyo.vue
+++ src/components/Weather/Tokyo.vue
@@ -1,27 +1,36 @@
 <template>
-    <v-card class="mx-auto" max-width="400">
-      <v-img class="white--text align-end" height="300px" :src="image_src">
-        <v-card-title>{{dataOutputName}}</v-card-title>
-      </v-img>
-      <v-card-subtitle class="pb-0 text--primary">
-        Now
-        <div v-show="dataOutputLoading">
-          <v-progress-circular indeterminate color="primary"></v-progress-circular>
-        </div>
-        <div v-show="!dataOutputLoading">{{dataOutputTemp}}℃</div>
-      </v-card-subtitle>
-      <v-card-text>
-        <div>Today's high temperature {{dataOutputMaxTemp}}℃</div>
-        <div>Lowest Temperature {{dataOutputMinTemp}}℃</div>
-        <div>The current weather {{dataOutputCondition}}</div>
-        <img v-bind:src="dataOutputicon">
-      </v-card-text>
-      <v-card-actions>
-        <v-btn color="orange" text>Share</v-btn>
-        <v-btn color="orange" text>Explore</v-btn>
-      </v-card-actions>
-    </v-card>
+  <v-card class="mx-auto" max-width="400">
+    <v-img class="white--text align-end" height="300px" :src="image_src">
+      <v-card-title>{{dataOutputName}}</v-card-title>
+    </v-img>
+    <v-card-subtitle class="pb-0 text--primary">
+    <img v-bind:src="dataOutputicon">
+      <div v-show="loading">
+        <v-progress-circular indeterminate color="primary"></v-progress-circular>
+      </div>
+      <div v-show="!dataOutputLoading">{{dataOutputTemp}}℃</div> 
+    </v-card-subtitle>
+    <v-card-text>
+      <div>Today's high temperature {{dataOutputMaxTemp}}℃</div>
+      <div>Lowest Temperature {{dataOutputMinTemp}}℃</div>
+      <div>The current weather {{dataOutputCondition.main }}</div>
+    </v-card-text>
+    <v-card-actions class="pt-0">
+      <v-spacer></v-spacer>
+      <v-btn icon @click="show = !show">
+        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+      </v-btn>
+    </v-card-actions>
+    <v-expand-transition>
+      <div v-show="show">
+         <v-btn text>Share
+          <v-icon color="blue">mdi-twitter</v-icon>
+        </v-btn>
+      </div>
+    </v-expand-transition>
+  </v-card>
 </template>
+
 
 
 <script>
@@ -29,6 +38,7 @@ export default {
   data() {
     return {
     image_src: require("../../assets/tokyo.png"),
+    show: false,
   }
 },
   computed: {
