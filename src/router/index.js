91d1c13f87a22548cb9d2534926c5aee3849cf7e
attoiumani <attoiumani0313@gmail.com>
2020-02-27 00:21:35 +0900

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -32,8 +32,8 @@ const routes = [
 
 const router = new VueRouter({
   routes,
-  mode: "history",
-  base: process.env.BASE_URL
+//  mode: "history",
+  //base: process.env.BASE_URL
 });
 
 export default router;
