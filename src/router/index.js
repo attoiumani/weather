6158e81d14cea20164aba5fc62bc5c3218d26f2a
attoiumani <attoiumani0313@gamil.com>
2020-02-27 17:11:28 +0900

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,12 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import singup from "../views/singup.vue";
+import signin from "../views/signin.vue";
 import wether from "../views/wether.vue";
 import home from "../views/home.vue";
 import About from "../views/About.vue";
+import firebase from 'firebase'
+
 
 Vue.use(VueRouter);
 
@@ -26,7 +29,13 @@ const routes = [
   {
     path: "/About",
     name: "About",
-    component: About
+    component: About,
+    meta: { requiresAuth: true }
+  },
+  {
+    path: "/signin",
+    name: "signin",
+    component: signin
   }
 ];
 
@@ -35,5 +44,27 @@ const router = new VueRouter({
 //  mode: "history",
   //base: process.env.BASE_URL
 });
+
+
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  if (requiresAuth) {
+    // このルートはログインされているかどうか認証が必要です。
+    // もしされていないならば、ログインページにリダイレクトします。
+    firebase.auth().onAuthStateChanged(function (user) {
+      if (user) {
+        next()
+      } else {
+        next({
+          path: '/signin',
+          query: { redirect: to.fullPath }
+        })
+      }
+    })
+  } else {
+    next() // next() を常に呼び出すようにしてください!
+  }
+})
 
 export default router;
