1a7847595f7ce98394df8ec6171be6607fc0d3d0
attoiumani <attoiumani0313@gamil.com>
2020-02-27 17:11:25 +0900

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-footer color="primary" dark app>
+    <v-footer color="primary" dark>
       <v-col class="text-center">
         {{new Date().getFullYear()}} —
         <strong>Vue-Vuerouter-Vuex-axios-Vuetify</strong>
