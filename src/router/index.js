df49d76809c952f0a6c9912857de36c34e41de48
attoiumani <attoiumani0313@gamil.com>
2020-02-28 11:36:21 +0900

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import signin from "../views/signin.vue";
 import wether from "../views/wether.vue";
 import home from "../views/home.vue";
 import About from "../views/About.vue";
-import firebase from 'firebase'
+
 
 
 Vue.use(VueRouter);
@@ -30,7 +30,6 @@ const routes = [
     path: "/About",
     name: "About",
     component: About,
-    meta: { requiresAuth: true }
   },
   {
     path: "/signin",
@@ -46,25 +45,5 @@ const router = new VueRouter({
 });
 
 
-
-router.beforeEach((to, from, next) => {
-  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
-  if (requiresAuth) {
-    // このルートはログインされているかどうか認証が必要です。
-    // もしされていないならば、ログインページにリダイレクトします。
-    firebase.auth().onAuthStateChanged(function (user) {
-      if (user) {
-        next()
-      } else {
-        next({
-          path: '/signin',
-          query: { redirect: to.fullPath }
-        })
-      }
-    })
-  } else {
-    next() // next() を常に呼び出すようにしてください!
-  }
-})
 
 export default router;
