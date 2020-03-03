19abc1b8b7a105cafd4fbd84175f79809455c341
attoiumani <attoiumani0313@gamil.com>
2020-03-03 10:44:33 +0900

    Update weather.vue

--- src/views/weather.vue
+++ src/views/weather.vue
@@ -1,38 +1,34 @@
 <template>
-    <v-row>
-      <v-col cols="12" sm="12" md="6" lg="4" xl="4">
-        <Tokyo/>
-      </v-col>
-      <v-col cols="12" sm="12" md="6" lg="4" xl="4">
-        <weather :place="place"/>
-      </v-col>
-      <v-col cols="12" sm="12" md="6" lg="4" xl="4">
-        <Fukuoka/>
-      </v-col>
-      <v-col cols="12" sm="12" md="6" lg="4" xl="4">
-        <Sapporo/>
-      </v-col>
-    </v-row>
+  <v-row>
+    <v-col cols="12" sm="12" md="6" lg="4" xl="4">
+      <Sapporo />
+    </v-col>
+    <v-col cols="12" sm="12" md="6" lg="4" xl="4">
+      <weather place="sendai" />
+    </v-col>
+    <v-col cols="12" sm="12" md="6" lg="4" xl="4">
+      <Tokyo />
+    </v-col>
+    <v-col cols="12" sm="12" md="6" lg="4" xl="4">
+      <weather place="osaka" />
+    </v-col>
+    <v-col cols="12" sm="12" md="6" lg="4" xl="4">
+      <weather place="fukuoka" />
+    </v-col>
+  </v-row>
 </template>
 
 
 <script>
 import Tokyo from "../components/Weather/Tokyo";
-import Fukuoka from "../components/Weather/Fukuoka";
 import Sapporo from "../components/Weather/Sapporo";
-import weather from "../components/Weather/weather";
+import weather from "../components/weather";
 
 export default {
-  data: function(){
-      return {
-          place: "osaka"
-      }
-  },
   components: {
     Tokyo,
-    Fukuoka,
     Sapporo,
     weather
   }
-  }
+};
 </script>
