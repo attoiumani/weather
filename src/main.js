fedc32c401c32d8367d43d14092263685d80b709
attoiumani <attoiumani0313@gamil.com>
2020-03-12 11:27:51 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import router from './router'
 import vuetify from './plugins/vuetify';
 import store from './store'
 import firebase from 'firebase'
-import 'firebase/firestore'
 
 Vue.config.productionTip = false
 
