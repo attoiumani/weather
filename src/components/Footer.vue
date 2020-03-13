ed4c48887e7594b241601d2b14c2dff46af8358e
attoiumani <attoiumani0313@gamil.com>
2020-03-13 11:31:52 +0900

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,9 +2,27 @@
   <div>
     <v-footer color="primary" dark>
       <v-col class="text-center">
-        {{new Date().getFullYear()}} —
+        {{YMT}} —
         <strong>Vue-Vuetify</strong>
       </v-col>
     </v-footer>
   </div>
 </template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      today: null
+    };
+  },
+  created: function() {
+    let now = new Date();
+    let Year = now.getFullYear();
+    let Month = now.getMonth()+1;
+    let Today = now.getDate();
+    this.YMT = Year+"/"+Month+"/"+Today;
+  }
+};
+</script>
