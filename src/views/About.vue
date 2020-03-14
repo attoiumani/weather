e4e83f82df4be846414a97d19cc5444af8bfcbff
attoiumani <attoiumani0313@gmail.com>
2020-03-14 20:44:47 +0900

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,33 +1,47 @@
 <template>
-  <div>
-    {{ allData }}
-  </div>
+  <div>{{ allData }}</div>
 </template>
 
 <script>
-import firebase from 'firebase'
+import firebase from "firebase";
 
 export default {
   data() {
     return {
-      allData: [],
-    }
+      db: null,
+      allData: null
+    };
   },
-  created() {
+  created: function() {
+    // この先にあるthenでthisの参照が切れるのでselfに退避させておく
 
-        firebase.firestore().collection("sapporo").get().then(snapshot => {
+    this.db = firebase.firestore();
+    // 直前に保存したデータを1件取得してくる
+    let collection = this.db.collection("sapporo");
+    let docRef = collection.doc("aaa");
+    docRef
+      .get()
+      .then(function(doc) {
+        if (doc.exists) {
+          console.log(doc.data().temp);
+        } else {
+          // doc.data() will be undefined in this case
+          console.log("No such document!");
+        }
+      })
+      .catch(function(error) {
+        console.log("Error getting document:", error);
+      });
+
+
+              firebase.firestore().collection("sapporo").get().then(snapshot => {
           snapshot.forEach(doc => {
             this.allData.push(doc.data().temp)
           })
         })
-
   },
-  methods: {
 
-  },
-  computed: {
-
-  },
-}
-
+  methods: {},
+  computed: {}
+};
 </script>
