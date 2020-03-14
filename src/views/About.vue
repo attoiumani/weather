15136bf4ffb90892a0ca23a11c761ec4a4026c43
attoiumani <attoiumani0313@gmail.com>
2020-03-14 13:18:38 +0900

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,38 +1,33 @@
 <template>
-  <div>{{tag}}</div>
+  <div>
+    {{ allData }}
+  </div>
 </template>
 
 <script>
-import axios from "axios";
+import firebase from 'firebase'
 
 export default {
   data() {
     return {
-      tag: String
-    };
+      allData: [],
+    }
   },
+  created() {
 
-  mounted: function() {
-    axios;
-    let base = "https://api.clashroyale.com/v1";
-    let token =
-      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjEzZmZmMGI0LWYyZjgtNDEwYy1hM2FkLWFkYjE1MmIyMTliYSIsImlhdCI6MTU4MDg5NjQ2OCwic3ViIjoiZGV2ZWxvcGVyLzgyNjM0MDRhLWQyMjctN2VmYS04YzMwLWYyNmE2NGViNmZkNSIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIyMTAuMTUwLjE0NC45NCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.MpYj9LPZNwTqbueinsb6wZUPTz0qtl1pLrZjr9YtyK6EtgoyClFJ0hVx2o9ydEHghtDGqJWvUw_oJi9euHtQJw";
-    let target_api = base + "/players/";
-    let playerTag = "%2328UGR809L";
-    let okurl = target_api + playerTag;
+        firebase.firestore().collection("sapporo").get().then(snapshot => {
+          snapshot.forEach(doc => {
+            this.allData.push(doc.data().temp)
+          })
+        })
 
-    return axios
-      .get(okurl, {
-        headers: {
-          "content-type": "application/json; charset=utf-8",
-          "cache-control": "max-age=60",
-          "Access-Control-Allow-Origin" : "*",
-          Authorization: `Bearer ${token}`
-        }
-      })
-      .then(function(response) {
-        this.tag = response.data;
-      });
-  }
-};
+  },
+  methods: {
+
+  },
+  computed: {
+
+  },
+}
+
 </script>
