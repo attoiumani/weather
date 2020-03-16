6a6bd1d8e1a4050c081b766dd9559bf19ba96f0f
attoiumani <attoiumani0313@gamil.com>
2020-03-16 16:23:34 +0900

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -9,15 +9,23 @@ export default {
   data() {
     return {
       db: null,
-      output: null
+      output: null,
+      YMT: null
     };
   },
   created: function() {
     let self = this;
 
+    let now = new Date();
+    let Year = now.getFullYear();
+    let Month = now.getMonth() + 1;
+    let Today = now.getDate();
+    this.YMT = Year + "" + Month + "" + Today;
+
+
     this.db = firebase.firestore();
     let collection = this.db.collection("kanazawa");
-    let docRef = collection.doc("2020316");
+    let docRef = collection.doc(this.YMT);
 
     docRef
       .get()
