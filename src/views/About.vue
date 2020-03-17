16ab3414bcb9dc7619310ddede47b4f6b9c8eb00
attoiumani <attoiumani0313@gamil.com>
2020-03-17 16:26:43 +0900

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -20,16 +20,16 @@ export default {
     let Year = now.getFullYear();
     let Month = now.getMonth() + 1;
     let Today = now.getDate();
+    let Past = now.getDate()-7;
     this.YMT = Year + "" + Month + "" + Today;
+    this.OWB = Year + "" + Month + "" + Past;
 
 
     this.db = firebase.firestore();
     let collection = this.db.collection("kanazawa");
     let docRef = collection.doc(this.YMT);
 
-    docRef
-      .get()
-      .then(function(doc) {
+    docRef.get().then(function(doc) {
         if (doc.exists) {
           self.output = doc.data().temp;
         } else {
