cac051ff330de02da80c0dfd1ee1e6d43f865d80
attoiumani <attoiumani0313@gamil.com>
2020-03-18 17:25:27 +0900

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,31 +1,33 @@
 <template>
-  <div>
-    {{ allData }}
-  </div>
+  <div>{{ allData }}</div>
 </template>
 
 <script>
-import firebase from 'firebase'
+import firebase from "firebase";
 
 export default {
   data() {
     return {
-      allData: [],
-    }
+      allData: []
+    };
   },
   created() {
+    this.db = firebase.firestore();
+
     let now = new Date();
     let Year = now.getFullYear();
     let Month = now.getMonth() + 1;
     let Today = now.getDate();
-    this.YMT = Year + "" + Month + "" + Today;
-        firebase.firestore().collection('kanazawa').where("YMT", "<", this.YMT).get().then(snapshot => {
-          snapshot.forEach(doc => {
-            this.allData.push(doc.data().temp)
-          })
-        })
-
-  },
-}
-
+    this.Today = Year + "" + Month + "" + Today;
+    this.db
+      .collection("kanazawa")
+      .where("Timestamp", "<", this.Today)
+      .get()
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+          this.allData.push(doc.data().temp);
+        });
+      });
+  }
+};
 </script>
