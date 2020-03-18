13bfe0919d898574a0aaa9fc8c4bee43f34e2e34
attoiumani <attoiumani0313@gamil.com>
2020-03-18 16:56:24 +0900

    Update Past.vue

--- src/components/Past.vue
+++ src/components/Past.vue
@@ -1,54 +1,34 @@
 <template>
-  <div>{{ output }}</div>
+  <div>
+    {{ allData }}
+  </div>
 </template>
 
 <script>
-import firebase from "firebase";
+import firebase from 'firebase'
 
 export default {
   data() {
     return {
-      output: [],
-      YMT: null
-    };
+      allData: [],
+    }
   },
-  created: function() {
-    let now = new Date();
-    let Year = now.getFullYear();
-    let Month = now.getMonth() + 1;
-    let Today = now.getDate();
-    let Past = now.getDate() - 7;
-    this.YMT = Year + "" + Month + "" + Today;
-    this.OWB = Year + "" + Month + "" + Past;
-
-    firebase
-      .firestore()
-      .collection("kanazawa")
-      .where("YMT", "=>", this.QWB)
-      .get()
-      .then(snapshot => {
-        snapshot.forEach(doc => {
-          this.output.push(doc.data().temp);
-        });
-      });
-  }
-};
+  created() {
 
 
+        firebase.firestore().collection('kanazawa').get().then(snapshot => {
+          snapshot.forEach(doc => {
+            this.allData.push(doc.data().temp)
+          })
+        })
 
-    docRef
-      .get()
-      .then(function(doc) {
-        if (doc.exists) {
-          self.output = doc.data().temp;
-        } else {
-          // doc.data() will be undefined in this case
-          console.log("No such document!");
-        }
-      })
-      .catch(function(error) {
-        console.log("Error getting document:", error);
-      });
-  }
-};
+  },
+  methods: {
+
+  },
+  computed: {
+
+  },
+}
+
 </script>
