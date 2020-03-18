d06f2e6717386aafa5872e6e7a51caebb9126c2e
attoiumani <attoiumani0313@gamil.com>
2020-03-18 17:03:03 +0900

    Update Past.vue

--- src/components/Past.vue
+++ src/components/Past.vue
@@ -1,26 +1,40 @@
 <template>
-  <div>
-    {{ allData }}
-  </div>
+  <div>{{ output }}</div>
 </template>
 
 <script>
-import firebase from 'firebase'
+import firebase from "firebase";
 
 export default {
   data() {
     return {
-      allData: [],
-    }
+      db: null,
+      output: null,
+      YMT: null
+    };
   },
-  created() {
-        firebase.firestore().collection('kanazawa').where("YMT", "==", "1").get().then(snapshot => {
-          snapshot.forEach(doc => {
-            this.allData.push(doc.data().temp)
-          })
-        })
+  created: function() {
+    let self = this;
 
-  },
-}
+    let now = new Date();
+    let Year = now.getFullYear();
+    let Month = now.getMonth() + 1;
+    let Today = now.getDate();
+    let Past = now.getDate() - 7;
+    this.YMT = Year + "" + Month + "" + Today;
+    this.OWB = Year + "" + Month + "" + Past;
 
+    this.db = firebase.firestore();
+    let collection = this.db.collection("kanazawa");
+    let docRef = collection.where("YMT", "<", this.YMT);
+
+    docRef
+      .get()
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+          self.output = doc.data().temp;
+        });
+      });
+  }
+};
 </script>
