cfc68fb7cdf8671d5e38dd17003ecd5549489e1f
attoiumani <attoiumani0313@gamil.com>
2020-03-25 09:55:04 +0900

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,77 +1,16 @@
-//about
-
 <template>
   <div>
-    <v-sparkline
-      :labels="labels"
-      :value="value"
-      :gradient="gradient"
-      :smooth="radius || false"
-      :padding="padding"
-      :line-width="width"
-      :stroke-linecap="lineCap"
-      :gradient-direction="gradientDirection"
-      :fill="fill"
-      :type="type"
-      :auto-line-width="autoLineWidth"
-      auto-draw
-      :label-size="labelSize"
-    ></v-sparkline>
-    <div>{{ value }}</div>
+    <h1>棒グラフと線グラフ</h1>
+    <chart></chart>
   </div>
 </template>
 
-
-
-
 <script>
-import firebase from "firebase";
-
-const gradients = [
-  ["#222"],
-  ["#42b3f4"],
-  ["red", "orange", "yellow"],
-  ["purple", "violet"],
-  ["#00c6ff", "#F0F", "#FF0"],
-  ["#f72047", "#ffd200", "#1feaea"]
-];
+import Chart from '@/components/Chart';
 
 export default {
-  data: () => {
-    return {
-      width: 2,
-      radius: 10,
-      padding: 8,
-      lineCap: "round",
-      gradient: gradients[5],
-      value: [],
-      labels: [],
-      gradientDirection: "top",
-      gradients,
-      fill: false,
-      type: "trend",
-      autoLineWidth: false,
-      labelSize: 2,
-    };
+  components: {
+    Chart,
   },
-  created() {
-    this.db = firebase.firestore();
-
-    let now = new Date();
-    let Year = now.getFullYear();
-    let Month = now.getMonth() + 1;
-    let Today = now.getDate();
-    this.Today = Year + "" + Month + "" + Today;
-    this.db
-      .collection("kanazawa")
-      .where("Timestamp", "<=", this.Today) //今日までのtempを取得
-      .get()
-      .then(snapshot => {
-        snapshot.forEach(doc => {
-          this.value.push(doc.data().temp);
-          this.labels.push(doc.data().Timestamp);
-        });
-      });
-  }
-};
+}
 </script>
