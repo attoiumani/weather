8f965cf99c1fc49517dbb5b29d2d8f6a8d31c9e4
attoiumani <attoiumani0313@gmail.com>
2020-05-02 22:08:31 +0900

    Update Past2.vue

--- src/components/past/Past2.vue
+++ src/components/past/Past2.vue
@@ -11,12 +11,26 @@ exports.clearoffers = functions.pubsub.schedule('30 12 * * *')
   .timeZone('Asia/Tokyo').onRun(async (context) => {
 
 
-    const r = await admin.firestore().collection('offers').listDocuments().then(val => {
-      val.map((val) => {
-        val.delete()
-      })
-    })
-
+      firebase
+        .firestore()
+        .collection(this.place) //props
+        .doc(this.Timestamp)  //tody
+        .set({
+          temp: this.temp,
+          maxtemp: this.maxtemp,
+          mintemp: this.mintemp,
+          Timestamp: this.Timestamp,
+          Timestamp2: this.Timestamp2
+        })
+        .then(function(docRef) {
+          // 正常にデータ保存できた時の処理
+          console.log("Document written with ID: ", docRef.id);
+        })
+        .catch(function(error) {
+          // エラー発生時の処理
+          console.error("Error adding document: ", error);
+        });
+ 
 
   });
 
@@ -70,27 +84,43 @@ function outPutImg(data) {
 
 
 
-      sendItem() {
-      firebase
-        .firestore()
-        .collection(this.place) //props
-        .doc(this.Timestamp)  //tody
-        .set({
-          temp: this.temp,
-          maxtemp: this.maxtemp,
-          mintemp: this.mintemp,
-          Timestamp: this.Timestamp,
-          Timestamp2: this.Timestamp2
-        })
-        .then(function(docRef) {
-          // 正常にデータ保存できた時の処理
-          console.log("Document written with ID: ", docRef.id);
-        })
-        .catch(function(error) {
-          // エラー発生時の処理
-          console.error("Error adding document: ", error);
-        });
-    }
+const http = require('http')
+const fs = require('fs')
+const request = require('request')
+const axios = require('axios');
+const ctype = {'Content-Type': 'text/html;charset=utf-8'}
 
+//APIの実行
+async function getData() {
+  const url = 'http://aikatsup.com/api/v1/search'
+  return await axios.get(url, {
+    params: {
+      id: 3144
+    }
+  })
+}
+
+//画像の保存
+function outPutImg(data) {
+  request(data.image.url).pipe(fs.createWriteStream('img/' +data.id+ '_' +data.words+ '.jpg'))
+}
+
+function handler (req, res) {
+  res.writeHead(200, ctype)
+
+  getData().then((data) => {
+    data = data.data.item
+    outPutImg(data)
+    resData = '<h1>' +data.id+ '_' + data.words + '</h1><img src=\"' +data.image.url+ '\" width=\"960\">'
+    res.end(resData)
+  }).catch(() => {
+    resData = 'api通信失敗'
+    res.end(resData)
+  })
+}
+
+const svr = http.createServer(handler)
+console.log('http://localhost:8000')
+svr.listen(8000)
 
   </script>>
