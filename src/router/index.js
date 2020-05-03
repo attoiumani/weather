5a56e616bc867415b666e780a7c54be53300257e
attoiumani <attoiumani0313@gmail.com>
2020-05-03 12:11:23 +0900

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import VueRouter from "vue-router";
 import signup from "../views/signup.vue";
 import signin from "../views/signin.vue";
 import weather from "../views/weather.vue";
-import home from "../views/home.vue";
+import profile from "../views/profile.vue";
 import About from "../views/About.vue";
 import only from "../views/only.vue";
 import firebase from 'firebase'
@@ -15,13 +15,13 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: home
-  },
-  {
-    path: "/weather",
     name: "weather",
     component: weather
+  },
+  {
+    path: "/profile",
+    name: "hoprofileme",
+    component: profile
   },
   {
     path: "/signup",
