f1ef7b0e7a26aa143cd447e9255ee292ac383b25
attoiumani <attoiumani0313@gmail.com>
2020-05-04 19:18:21 +0900

    Update linechart.vue

--- src/components/chart/linechart.vue
+++ src/components/chart/linechart.vue
@@ -1,53 +1,52 @@
 <script>
-import { Line } from "vue-chartjs";
+import { Bar } from "vue-chartjs";
 import firebase from "firebase";
 import moment from "moment";
 
 export default {
-  extends: Line,
+  extends: Bar,
+  name: "chart",
   data() {
     return {
-      gradient: null
+      data: {
+        labels: [],
+        datasets: [
+          {
+            label: "℃",
+            data: [],
+            borderWidth: 1,
+            borderColor: "#FC2525",
+            pointBackgroundColor: "rgba(255, 0,0, 0.5)",
+            pointBorderColor: "white",
+            backgroundColor: "rgba(255, 0,0, 0.5)",
+            type: "line"
+          }
+        ]
+      },
     };
   },
   mounted() {
-    let chartData = {
-      labels: [],
-      datasets: [
-        {
-          label: "Data One",
-          borderColor: "#FC2525",
-          pointBackgroundColor: "white",
-          borderWidth: 1,
-          pointBorderColor: "white",
-          backgroundColor: this.gradient,
-          data: []
-        }
-      ]
-    };
-
-    this.renderChart(chartData, {
-      responsive: true,
-      maintainAspectRatio: false
-    });
-
-    this.db = firebase.firestore();
-
     let m = moment();
     let Year = m.format("YYYY");
     let Month = m.format("MM");
     let day = m.format("DD");
     this.Today = Year + "" + Month + "" + day;
+    this.db = firebase.firestore();
     this.db
       .collection(this.place)
       .where("Timestamp", "<=", this.Today) //今日までのtempを取得
       .get()
       .then(snapshot => {
         snapshot.forEach(doc => {
-          chartData.datasets[0].data.push(doc.data().temp);
-          chartData.labels.push(doc.data().Timestamp2);
+          this.data.datasets[0].data.push(doc.data().temp);
+          this.data.labels.push(doc.data().Timestamp2);
         });
       });
-  }
+    this.renderChart(this.data, {
+      responsive: true,
+      maintainAspectRatio: false
+    });
+  },
+  props: ["place"]
 };
 </script>
