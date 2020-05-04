a810a1f12333e40d9ee58c796e05cae367fedd6b
attoiumani <attoiumani0313@gmail.com>
2020-05-04 18:50:54 +0900

    Update linechart.vue

--- src/components/chart/linechart.vue
+++ src/components/chart/linechart.vue
@@ -26,14 +26,12 @@ export default {
     };
   },
   mounted() {
-    this.renderChart(this.data);
-    this.db = firebase.firestore();
-
     let m = moment();
     let Year = m.format("YYYY");
     let Month = m.format("MM");
     let day = m.format("DD");
     this.Today = Year + "" + Month + "" + day;
+    this.db = firebase.firestore();
     this.db
       .collection(this.place)
       .where("Timestamp", "<=", this.Today) //今日までのtempを取得
@@ -44,8 +42,8 @@ export default {
           this.data.labels.push(doc.data().Timestamp2);
         });
       });
+    this.renderChart(this.data);
   },
-
   props: ["place"]
 };
 </script>
