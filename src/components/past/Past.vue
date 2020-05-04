39e0e3df8e3d18afac7f8d3e6b046f6d4e5e6682
attoiumani <attoiumani0313@gmail.com>
2020-05-04 19:18:39 +0900

    Update Past.vue

--- src/components/past/Past.vue
+++ src/components/past/Past.vue
@@ -1,53 +1,33 @@
+
 <script>
 import { Line } from "vue-chartjs";
-import firebase from "firebase";
-import moment from "moment";
 
 export default {
   extends: Line,
-  data() {
-    return {
-      gradient: null
-    };
-  },
+  name: "LineChart",
+  props: ["data"],
   mounted() {
+    // reformat in the way the lib wants
     let chartData = {
       labels: [],
       datasets: [
         {
-          label: "Data One",
-          borderColor: "#FC2525",
-          pointBackgroundColor: "white",
-          borderWidth: 1,
-          pointBorderColor: "white",
-          backgroundColor: this.gradient,
+          label: "Page Views",
+          backgroundColor: "#f87979",
           data: []
         }
       ]
     };
 
+    for (let i = 0; i < this.data.length; i++) {
+      chartData.labels.push(this.data[i].date);
+      chartData.datasets[0].data.push(this.data[i].views);
+    }
+
     this.renderChart(chartData, {
       responsive: true,
       maintainAspectRatio: false
     });
-
-    this.db = firebase.firestore();
-
-    let m = moment();
-    let Year = m.format("YYYY");
-    let Month = m.format("MM");
-    let day = m.format("DD");
-    this.Today = Year + "" + Month + "" + day;
-    this.db
-      .collection(this.place)
-      .where("Timestamp", "<=", this.Today) //今日までのtempを取得
-      .get()
-      .then(snapshot => {
-        snapshot.forEach(doc => {
-          chartData.datasets[0].data.push(doc.data().temp);
-          chartData.labels.push(doc.data().Timestamp2);
-        });
-      });
   }
 };
 </script>
