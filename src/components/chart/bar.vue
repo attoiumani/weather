9a634a354746206c3a15e45b99ecbf9229f5483b
attoiumani <attoiumani0313@gmail.com>
2020-05-05 12:04:23 +0900

    Create bar.vue

---
+++ src/components/chart/bar.vue
@@ -0,0 +1,51 @@
+<script>
+import { Bar } from "vue-chartjs";
+import firebase from "firebase";
+import moment from "moment";
+
+export default {
+  extends: Bar,
+  data() {
+    return {
+      data: {
+        labels: [],
+        datasets: [
+          {
+            label: "℃",
+            data: [],
+            borderWidth: 1,
+            borderColor: "#FC2525",
+            pointBackgroundColor: "rgba(255, 0,0, 0.5)",
+            pointBorderColor: "white",
+            backgroundColor: "rgba(255, 0,0, 0.5)",
+          }
+        ]
+      },
+    };
+  },
+  mounted() {
+    this.renderChart(this.data, {
+      responsive: true,
+      maintainAspectRatio: false
+    });
+
+    let m = moment();
+    let Year = m.format("YYYY");
+    let Month = m.format("MM");
+    let day = m.format("DD");
+    this.Today = Year + "" + Month + "" + day;
+    this.db = firebase.firestore();
+    this.db
+      .collection(this.place)
+      .where("Timestamp", "<=", this.Today) //今日までのtempを取得
+      .get()
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+          this.data.datasets[0].data.push(doc.data().temp);
+          this.data.labels.push(doc.data().Timestamp2);
+        });
+      });
+  },
+  props: ["place"]
+};
+</script>
