55d16f30d0b3a544b781ae7d9f0cccddb01f84ed
attoiumani <attoiumani0313@gmail.com>
2020-05-05 12:11:09 +0900

    Create analytics.vue

---
+++ src/views/analytics.vue
@@ -0,0 +1,24 @@
+<template>
+<div id="app">
+  <linechart place="osaka" />
+  <piechart place="osaka" />
+  <bar place="osaka" />
+  <radar place="osaka" />
+  </div>
+</template>
+
+<script>
+import linechart from "@/components/chart/linechart";
+import piechart from "@/components/chart/piechart";
+import radar from "@/components/chart/radar";
+import bar from "@/components/chart/bar";
+
+export default {
+  components: {
+    linechart,
+    piechart,
+    radar,
+    bar,
+  }
+};
+</script>
