078fdbbe3b590acd66e29d717ce4673a492e90b9
attoiumani <attoiumani0313@gmail.com>
2020-05-05 12:04:36 +0900

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,17 +2,24 @@
 <div id="app">
   <linechart place="osaka" />
   <piechart place="osaka" />
+  <bar place="osaka" />
+  <radar place="osaka" />
   </div>
 </template>
 
 <script>
 import linechart from "@/components/chart/linechart";
 import piechart from "@/components/chart/piechart";
+import radar from "@/components/chart/radar";
+import bar from "@/components/chart/bar";
 
 export default {
   components: {
     linechart,
-    piechart
+    piechart,
+    radar,
+    bar,
+
   }
 };
 </script>
