4e9ca35ca5dc609b5208bf00579b6d629b026125
attoiumani <attoiumani0313@gmail.com>
2020-05-07 13:41:35 +0900

    Create chart.vue

---
+++ src/views/chart.vue
@@ -0,0 +1,16 @@
+<template>
+<div id="app">
+  <allchart/>
+  </div>
+</template>
+
+<script>
+import allchart from "@/components/chart/allchart";
+
+
+export default {
+  components: {
+    allchart,
+  }
+};
+</script>
