2819c10fd7ab33f0867b343f87ba2392276ae9d7
attoiumani <49916591+attoiumani@users.noreply.github.com>
2020-05-08 15:51:15 +0900

    Update Past.vue

--- src/components/past/Past.vue
+++ src/components/past/Past.vue
@@ -1,33 +1,64 @@
-
 <script>
 import { Line } from "vue-chartjs";
 
 export default {
   extends: Line,
-  name: "LineChart",
-  props: ["data"],
-  mounted() {
-    // reformat in the way the lib wants
-    let chartData = {
-      labels: [],
-      datasets: [
-        {
-          label: "Page Views",
-          backgroundColor: "#f87979",
-          data: []
-        }
-      ]
+  data() {
+    return {
+      gradient: null,
+      gradient2: null
     };
+  },
+  mounted() {
+    this.gradient = this.$refs.canvas
+      .getContext("2d")
+      .createLinearGradient(0, 0, 0, 450);
+    this.gradient2 = this.$refs.canvas
+      .getContext("2d")
+      .createLinearGradient(0, 0, 0, 450);
 
-    for (let i = 0; i < this.data.length; i++) {
-      chartData.labels.push(this.data[i].date);
-      chartData.datasets[0].data.push(this.data[i].views);
-    }
+    this.gradient.addColorStop(0, "rgba(255, 0,0, 0.5)");
+    this.gradient.addColorStop(0.5, "rgba(255, 0, 0, 0.25)");
+    this.gradient.addColorStop(1, "rgba(255, 0, 0, 0)");
 
-    this.renderChart(chartData, {
-      responsive: true,
-      maintainAspectRatio: false
-    });
+    this.gradient2.addColorStop(0, "rgba(0, 231, 255, 0.9)");
+    this.gradient2.addColorStop(0.5, "rgba(0, 231, 255, 0.25)");
+    this.gradient2.addColorStop(1, "rgba(0, 231, 255, 0)");
+
+    this.renderChart(
+      {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July"
+        ],
+        datasets: [
+          {
+            label: "Data One",
+            borderColor: "#FC2525",
+            pointBackgroundColor: "white",
+            borderWidth: 1,
+            pointBorderColor: "white",
+            backgroundColor: this.gradient,
+            data: [40, 39, 10, 40, 39, 80, 40]
+          },
+          {
+            label: "Data Two",
+            borderColor: "#05CBE1",
+            pointBackgroundColor: "white",
+            pointBorderColor: "white",
+            borderWidth: 1,
+            backgroundColor: this.gradient2,
+            data: [60, 55, 32, 10, 2, 12, 53]
+          }
+        ]
+      },
+      { responsive: true, maintainAspectRatio: false }
+    );
   }
 };
 </script>
