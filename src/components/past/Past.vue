55a052099130cb9f3ca39b7a89c9ca967bc086fa
attoiumani <49916591+attoiumani@users.noreply.github.com>
2020-05-08 15:53:01 +0900

    Update Past.vue

--- src/components/past/Past.vue
+++ src/components/past/Past.vue
@@ -61,4 +61,48 @@ export default {
     );
   }
 };
+
+
+
+
+
+
+
+
+
+import { Line } from "vue-chartjs";
+
+export default {
+  extends: Line,
+  name: "LineChart",
+  props: ["data"],
+  mounted() {
+    // reformat in the way the lib wants
+    let chartData = {
+      labels: [],
+      datasets: [
+        {
+          label: "Page Views",
+          backgroundColor: "#f87979",
+          data: []
+        }
+      ]
+    };
+
+    for (let i = 0; i < this.data.length; i++) {
+      chartData.labels.push(this.data[i].date);
+      chartData.datasets[0].data.push(this.data[i].views);
+    }
+
+    this.renderChart(chartData, {
+      responsive: true,
+      maintainAspectRatio: false
+    });
+  }
+};
+
+
 </script>
+
+
+
