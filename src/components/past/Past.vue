5d6835d90ef14e0edc3993fc612ce3a70ad4bdc4
attoiumani <49916591+attoiumani@users.noreply.github.com>
2020-05-20 16:06:45 +0900

    Update Past.vue

--- src/components/past/Past.vue
+++ src/components/past/Past.vue
@@ -1,64 +1,118 @@
+<!--testコンポーネントsapporo一週間とる-->
+
+<template>
+  <v-card class="mx-auto" max-width="400">
+    <v-img class="white--text align-end" height="300px" :src="image_src">
+      <v-card-title>{{city}}</v-card-title>
+    </v-img>
+    <v-card-subtitle class="pb-0 text--primary">
+      <img v-bind:src="icon" />
+      <div v-show="loading">
+        <v-progress-circular indeterminate color="primary"></v-progress-circular>
+      </div>
+      <div v-show="!loading">{{temp}}℃</div>
+    </v-card-subtitle>
+    <v-card-text>
+      <div>Today's high temperature {{temp}}℃</div>
+      <div>Lowest Temperature {{mintemp}}℃</div>
+      <div>The current weather {{condition.main }}</div>
+    </v-card-text>
+    <v-card-actions class="pt-0">
+      <v-spacer></v-spacer>
+      <v-btn icon @click="show = !show">
+        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+      </v-btn>
+    </v-card-actions>
+    <v-expand-transition>
+      <div v-show="show">
+        <v-btn @click="twitterShare" text>
+          Share
+          <v-icon color="blue">mdi-twitter</v-icon>
+        </v-btn>
+        <v-btn @click="sendItem" text>
+          firestore
+          <v-icon color="orange">mdi-firebase</v-icon>
+        </v-btn>
+      </div>
+    </v-expand-transition>
+  </v-card>
+</template>
+
+
 <script>
-import { Line } from "vue-chartjs";
+import axios from "axios";
+import firebase from "firebase";
 
 export default {
-  extends: Line,
   data() {
     return {
-      gradient: null,
-      gradient2: null
+      image_src: require("@/assets/images/sapporo.png"),
+      city: null,
+      temp: null,
+      maxtemp: null,
+      mintemp: null,
+      condition: {
+        main: null
+      },
+      loading: true,
+      show: false,
+      YMT: null
     };
   },
-  mounted() {
-    this.gradient = this.$refs.canvas
-      .getContext("2d")
-      .createLinearGradient(0, 0, 0, 450);
-    this.gradient2 = this.$refs.canvas
-      .getContext("2d")
-      .createLinearGradient(0, 0, 0, 450);
-
-    this.gradient.addColorStop(0, "rgba(255, 0,0, 0.5)");
-    this.gradient.addColorStop(0.5, "rgba(255, 0, 0, 0.25)");
-    this.gradient.addColorStop(1, "rgba(255, 0, 0, 0)");
-
-    this.gradient2.addColorStop(0, "rgba(0, 231, 255, 0.9)");
-    this.gradient2.addColorStop(0.5, "rgba(0, 231, 255, 0.25)");
-    this.gradient2.addColorStop(1, "rgba(0, 231, 255, 0)");
-
-    this.renderChart(
-      {
-        labels: [
-          "January",
-          "February",
-          "March",
-          "April",
-          "May",
-          "June",
-          "July"
-        ],
-        datasets: [
-          {
-            label: "Data One",
-            borderColor: "#FC2525",
-            pointBackgroundColor: "white",
-            borderWidth: 1,
-            pointBorderColor: "white",
-            backgroundColor: this.gradient,
-            data: [40, 39, 10, 40, 39, 80, 40]
-          },
-          {
-            label: "Data Two",
-            borderColor: "#05CBE1",
-            pointBackgroundColor: "white",
-            pointBorderColor: "white",
-            borderWidth: 1,
-            backgroundColor: this.gradient2,
-            data: [60, 55, 32, 10, 2, 12, 53]
-          }
-        ]
-      },
-      { responsive: true, maintainAspectRatio: false }
-    );
+  created: function() {
+    axios
+            .get(
+        "http://api.openweathermap.org/data/2.5/forecast?q=Tokyo&APPID=4dff50a83aa2145ba555d8f59e9d3ef0"
+      )
+      .then(
+        function(response) {
+          this.temp = response.data.list[0].main.temp;
+          this.condition = response.data.weather[0];
+          this.loading = false;
+        }.bind(this)
+      );
+    let now = new Date();
+    let Year = now.getFullYear();
+    let Month = now.getMonth() + 1;
+    let Today = now.getDate();
+    this.YMT = Year + "/" + Month + "/" + Today;
+  },
+  methods: {
+    twitterShare() {
+      //twitter share
+      var shareURL =
+        "https://twitter.com/intent/tweet?text=" +
+        this.city +
+        this.temp +
+        "%20%23今の温度";
+      //シェア用の画面へ移行
+      location.href = shareURL;
+    },
+    sendItem() {
+      //firebase firestore
+      const saveData = {
+        temp: this.temp,
+        maxtemp: this.maxtemp,
+        mintemp: this.mintemp
+      };
+      firebase
+        .firestore()
+        .collection("sapporo")
+        .doc(this.YMT)
+        .set({
+          temp: saveData.temp,
+          maxtemp: saveData.maxtemp,
+          mintemp: saveData.mintemp
+        })
+        .then(function(docRef) {
+          // 正常にデータ保存できた時の処理
+          console.log("Document written with ID: ", docRef.id);
+        })
+        .catch(function(error) {
+          // エラー発生時の処理
+          console.error("Error adding document: ", error);
+        });
+    }
   }
 };
 </script>
