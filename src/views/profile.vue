4f5479e7eb98a56c244260ebb4309413154b5cb0
attoiumani <attoiumani0313@gmail.com>
2020-05-24 20:44:52 +0900

    Update profile.vue

--- src/views/profile.vue
+++ src/views/profile.vue
@@ -1,5 +1,9 @@
 <template>
-    <div>
-      <div class="text-center" id="nav">T</div>
-    </div>
+  <div>
+    <div class="text-center" id="nav">フロントエンド : vue vue-router vuex</div>
+    <div class="text-center" id="nav">ライブラリ : vuetify moment.js chart.js</div>
+    <div class="text-center" id="nav">使用API : openweathermap</div>
+    <div class="text-center" id="nav">データベース : firestore</div>
+    <div class="text-center" id="nav">サーバサイド : firebase function node.js</div>
+  </div>
 </template>
