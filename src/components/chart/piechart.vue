ebe2e0bed863c90c3311fed88f1a218254503203
attoiumani <attoiumani0313@gamil.com>
2020-06-01 16:12:06 +0900

    Update piechart.vue

--- src/components/chart/piechart.vue
+++ src/components/chart/piechart.vue
@@ -1,50 +1,21 @@
 <script>
-import { Pie } from "vue-chartjs";
-import firebase from "firebase";
-import moment from "moment";
-
-export default {
-  extends: Pie,
-  data() {
-    return {
-      data: {
-        labels: ["rain","sun","clouds","snow"],
-        datasets: [
-          {
-            label: "℃",
-            data: [],
-            borderWidth: 1,
-            borderColor: "#FC2525",
-            pointBackgroundColor: "rgba(255, 0,0, 0.5)",
-            pointBorderColor: "white",
-            backgroundColor: "rgba(255, 0,0, 0.5)",
-          }
-        ]
-      },
-    };
-  },
+import { Line } from "vue-chartjs";
+const MyChart = {
+  extends: Line,
+  props: ["data", "options"],
   mounted() {
-    this.renderChart(this.data, {
-      responsive: true,
-      maintainAspectRatio: false
-    });
-
-    let m = moment();
-    let Year = m.format("YYYY");
-    let Month = m.format("MM");
-    let day = m.format("DD");
-    this.Today = Year + "" + Month + "" + day;
-    this.db = firebase.firestore();
-    this.db
-      .collection(this.place)
-      .where("Timestamp", "<=", this.Today) //今日までのtempを取得
-      .get()
-      .then(snapshot => {
-        snapshot.forEach(doc => {
-          this.data.datasets[0].data.push(doc.data().condition);
-        });
-      });
+    this.renderChart(this.data || this.chartData, this.options);
   },
-  props: ["place"]
+  watch: {
+    data: {
+      handler() {
+        this.renderChart(this.data, this.options);
+        // for more errors uncomment this instead:
+        // this.$data._chart.update();
+      },
+      deep: true
+    }
+  }
 };
+export default MyChart;
 </script>
