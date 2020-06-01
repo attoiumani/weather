1ff9c9ff8f93c916644027ddecb56168baa9c3fb
attoiumani <attoiumani0313@gamil.com>
2020-06-01 15:34:40 +0900

    Update chart.vue

--- src/views/chart.vue
+++ src/views/chart.vue
@@ -1,16 +1,119 @@
 <template>
-<div id="app">
-  <allchart/>
-  </div>
+  <allchart :data="data" :options="options" />
 </template>
 
 <script>
+import firebase from "firebase";
+import moment from "moment";
 import allchart from "@/components/chart/allchart";
-
 
 export default {
   components: {
-    allchart,
+    allchart
+  },
+  data() {
+    return {
+      Today: null,
+      data: {
+        labels: [],
+        datasets: [
+          {
+            label: "osaka",
+            data: [],
+            backgroundColor: "transparent",
+            borderColor: "rgba(1, 116, 188, 0.50)",
+            pointBackgroundColor: "rgba(171, 71, 188, 1)"
+          },
+
+          {
+            label: "tokyo",
+            data: [],
+            backgroundColor: "transparent",
+            borderColor: "red",
+            pointBackgroundColor: "red"
+          },
+          {
+            label: "kanazawa",
+            data: [],
+            backgroundColor: "transparent",
+            borderColor: "green",
+            pointBackgroundColor: "green"
+          },
+          {
+            label: "fukuoka",
+            data: [],
+            backgroundColor: "transparent",
+            borderColor: "yellow",
+            pointBackgroundColor: "yellow"
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          xAxes: [
+            {
+              gridLines: {
+                display: true,
+                color: "rgba(0, 0, 0, 0.1)"
+              }
+            }
+          ],
+          yAxes: [
+            {
+              gridLines: {
+                display: true,
+                color: "rgba(0, 0, 0, 0.1)"
+              }
+            }
+          ]
+        }
+      }
+    };
+  },
+  created: function() {
+    let m = moment();
+    let Year = m.format("YYYY");
+    let Month = m.format("MM");
+    let day = m.format("DD");
+    this.Today = Year + "" + Month + "" + day;
+    let db = firebase.firestore();
+
+    db.collection("osaka")
+      .where("Timestamp", "<=", this.Today) //今日までのtempを取得
+      .get()
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+          this.data.datasets[0].data.push(doc.data().temp);
+          this.data.labels.push(doc.data().Timestamp2);
+        });
+      });
+
+    db.collection("tokyo")
+      .where("Timestamp", "<=", this.Today) //今日までのtempを取得
+      .get()
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+          this.data.datasets[1].data.push(doc.data().temp);
+        });
+      });
+    db.collection("kanazawa")
+      .where("Timestamp", "<=", this.Today) //今日までのtempを取得
+      .get()
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+          this.data.datasets[2].data.push(doc.data().temp);
+        });
+      });
+    db.collection("fukuoka")
+      .where("Timestamp", "<=", this.Today) //今日までのtempを取得
+      .get()
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+          this.data.datasets[3].data.push(doc.data().temp);
+        });
+      });
   }
 };
 </script>
