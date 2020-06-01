ee2e9b7c33009e56465adf08aa9a1ad12a1fd538
attoiumani <attoiumani0313@gamil.com>
2020-06-01 15:34:38 +0900

    Update allchart.vue

--- src/components/chart/allchart.vue
+++ src/components/chart/allchart.vue
@@ -1,103 +1,21 @@
 <script>
-import { Line ,mixins } from "vue-chartjs";
-import firebase from "firebase";
-import moment from "moment";
-
-export default {
+import { Line } from "vue-chartjs";
+const MyChart = {
   extends: Line,
-  mixins: [mixins.reactiveProp],
-  data() {
-    return {
-      Today: null,
-      data: {
-        labels: [],
-        datasets: [
-          {
-            label: "osaka",
-            data: [],
-            backgroundColor: "transparent",
-            borderColor: "rgba(1, 116, 188, 0.50)",
-            pointBackgroundColor: "rgba(171, 71, 188, 1)"
-          },
-
-          {
-            label: "tokyo",
-            data: [],
-            backgroundColor: "transparent",
-            borderColor: "red",
-            pointBackgroundColor: "red"
-          },
-          {
-            label: "kanazawa",
-            data: [],
-            backgroundColor: "transparent",
-            borderColor: "green",
-            pointBackgroundColor: "green"
-          },
-          {
-            label: "fukuoka",
-            data: [],
-            backgroundColor: "transparent",
-            borderColor: "yellow",
-            pointBackgroundColor: "yellow"
-          }
-        ]
-      }
-    };
-  },
-
+  props: ["data", "options"],
   mounted() {
-    this.renderChart(this.data, {
-      responsive: true,
-      maintainAspectRatio: false
-    });
+    this.renderChart(this.data || this.chartData, this.options);
   },
-
-  created: function() {
-    let m = moment();
-    let Year = m.format("YYYY");
-    let Month = m.format("MM");
-    let day = m.format("DD");
-    this.Today = Year + "" + Month + "" + day;
-    let db = firebase.firestore();
-
-    db.collection("osaka")
-      .where("Timestamp", "<=", this.Today) //今日までのtempを取得
-      .get()
-      .then(snapshot => {
-        snapshot.forEach(doc => {
-          this.data.datasets[0].data.push(doc.data().temp);
-          this.data.labels.push(doc.data().Timestamp2);
-        });
-      });
-
-    db
-      .collection("tokyo")
-      .where("Timestamp", "<=", this.Today) //今日までのtempを取得
-      .get()
-      .then(snapshot => {
-        snapshot.forEach(doc => {
-          this.data.datasets[1].data.push(doc.data().temp);
-        });
-      });
-    db
-      .collection("kanazawa")
-      .where("Timestamp", "<=", this.Today) //今日までのtempを取得
-      .get()
-      .then(snapshot => {
-        snapshot.forEach(doc => {
-          this.data.datasets[2].data.push(doc.data().temp);
-        });
-      });
-    db
-      .collection("fukuoka")
-      .where("Timestamp", "<=", this.Today) //今日までのtempを取得
-      .get()
-      .then(snapshot => {
-        snapshot.forEach(doc => {
-          this.data.datasets[3].data.push(doc.data().temp);
-        });
-      });
+  watch: {
+    data: {
+      handler() {
+        this.renderChart(this.data, this.options);
+        // for more errors uncomment this instead:
+        // this.$data._chart.update();
+      },
+      deep: true
+    }
   }
 };
+export default MyChart;
 </script>
