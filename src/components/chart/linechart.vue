2472efe07e67325b1270350eaa18f362331da651
attoiumani <attoiumani0313@gmail.com>
2020-07-03 13:29:57 +0900

    Update linechart.vue

--- src/components/chart/linechart.vue
+++ src/components/chart/linechart.vue
@@ -1,86 +1,21 @@
 <script>
 import { Line } from "vue-chartjs";
-import firebase from "firebase";
-import moment from "moment";
-
-export default {
+const MyChart = {
   extends: Line,
-  data() {
-    return {
-      gradient: null,
-      gradient2: null
-    };
-  },
+  props: ["data", "options"],
   mounted() {
-    this.gradient = this.$refs.canvas
-      .getContext("2d")
-      .createLinearGradient(0, 0, 0, 450);
-    this.gradient2 = this.$refs.canvas
-      .getContext("2d")
-      .createLinearGradient(0, 0, 0, 450);
-
-    this.gradient.addColorStop(0, "rgba(255, 0,0, 0.5)");
-    this.gradient.addColorStop(0.5, "rgba(255, 0, 0, 0.25)");
-    this.gradient.addColorStop(1, "rgba(255, 0, 0, 0)");
-
-    this.gradient2.addColorStop(0, "rgba(0, 231, 255, 0.9)");
-    this.gradient2.addColorStop(0.5, "rgba(0, 231, 255, 0.25)");
-    this.gradient2.addColorStop(1, "rgba(0, 231, 255, 0)");
-
-    let chartData = {
-        labels: [],
-        datasets: [
-          {
-            label: "max",
-            borderColor: "#FC2525",
-            pointBackgroundColor: "white",
-            borderWidth: 1,
-            pointBorderColor: "white",
-            backgroundColor: this.gradient,
-            data: []
-          },
-          {
-            label: "min",
-            borderColor: "#05CBE1",
-            pointBackgroundColor: "white",
-            pointBorderColor: "white",
-            borderWidth: 1,
-            backgroundColor: this.gradient2,
-            data: []
-          }
-        ]
-      };
-
-    this.renderChart(chartData, {
-      responsive: true,
-      maintainAspectRatio: false
-    });
-
-        let m = moment();
-    let Year = m.format("YYYY");
-    let Month = m.format("MM");
-    let day = m.format("DD");
-    this.Today = Year + "" + Month + "" + day;
-    this.db = firebase.firestore();
-
-    this.db
-      .collection(this.place)
-      .where("Timestamp", "<=", this.Today) //今日までのtempを取得
-      .get()
-      .then(snapshot => {
-        snapshot.forEach(doc => {
-          chartData.datasets[0].data.push(doc.data().maxtemp);
-          chartData.datasets[1].data.push(doc.data().mintemp);
-          chartData.labels.push(doc.data().Timestamp2);
-        });
-      });
+    this.renderChart(this.data , this.options);
   },
-
-
-    props: {
-      place:String,
-    },
-
-
+  /*watch: {
+    data: {
+      handler() {
+        this.renderChart(this.data, this.options);
+        // for more errors uncomment this instead:
+        // this.$data._chart.update();
+      },
+      deep: true
+    }
+  }*/
 };
+export default MyChart;
 </script>
