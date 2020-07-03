3da242f912fc96f13682a9f9182a2e04644880e6
attoiumani <attoiumani0313@gmail.com>
2020-07-03 13:29:53 +0900

    Update bar.vue

--- src/components/chart/bar.vue
+++ src/components/chart/bar.vue
@@ -1,51 +1,21 @@
 <script>
 import { Bar } from "vue-chartjs";
-import firebase from "firebase";
-import moment from "moment";
-
-export default {
+const MyChart = {
   extends: Bar,
-  data() {
-    return {
-      data: {
-        labels: [],
-        datasets: [
-          {
-            label: "℃",
-            data: [],
-            borderWidth: 1,
-            borderColor: "#FC2525",
-            pointBackgroundColor: "rgba(255, 0,0, 0.5)",
-            pointBorderColor: "white",
-            backgroundColor: "rgba(255, 0,0, 0.5)",
-          }
-        ]
-      },
-    };
-  },
+  props: ["data", "options"],
   mounted() {
-    this.renderChart(this.data, {
-      responsive: true,
-      maintainAspectRatio: false
-    });
-
-    let m = moment();
-    let Year = m.format("YYYY");
-    let Month = m.format("MM");
-    let day = m.format("DD");
-    this.Today = Year + "" + Month + "" + day;
-    this.db = firebase.firestore();
-    this.db
-      .collection(this.place)
-      .where("Timestamp", "<=", this.Today) //今日までのtempを取得
-      .get()
-      .then(snapshot => {
-        snapshot.forEach(doc => {
-          this.data.datasets[0].data.push(doc.data().temp);
-          this.data.labels.push(doc.data().Timestamp2);
-        });
-      });
+    this.renderChart(this.data , this.options);
   },
-  props: ["place"]
+  /*watch: {
+    data: {
+      handler() {
+        this.renderChart(this.data, this.options);
+        // for more errors uncomment this instead:
+        // this.$data._chart.update();
+      },
+      deep: true
+    }
+  }*/
 };
+export default MyChart;
 </script>
