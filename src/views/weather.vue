e22ba294ebf530f71a7544a55f9b608b96842729
attoiumani <attoiumani0313@gmail.com>
2020-07-04 11:31:32 +0900

    Update weather.vue

--- src/views/weather.vue
+++ src/views/weather.vue
@@ -3,9 +3,6 @@
     <v-col cols="12" sm="12" md="6" lg="4" xl="4">
       <Sapporo />
     </v-col>
-        <v-col cols="12" sm="12" md="6" lg="4" xl="4">
-      <tokyo />
-    </v-col>
     <v-col cols="12" sm="12" md="6" lg="4" xl="4">
       <weather place="sendai" />
     </v-col>
@@ -26,14 +23,13 @@
 
 
 <script>
-import Sapporo from "@/components/Weather/Sapporo";// Spporo components is Special!!
+import Sapporo from "@/components/Weather/Sapporo"; // Spporo components is Special!!
 import weather from "@/components/weather";
-import tokyo from "@/components/Weather/Tokyo";
+
 export default {
   components: {
     Sapporo,
-    weather,
-    tokyo
+    weather
   }
 };
 </script>
