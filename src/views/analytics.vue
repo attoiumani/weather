26c2a33bc2763dacdb0b8b8a2380c491eedc42e0
attoiumani <attoiumani0313@gmail.com>
2020-07-08 13:27:13 +0900

    Update analytics.vue

--- src/views/analytics.vue
+++ src/views/analytics.vue
@@ -76,17 +76,35 @@ export default {
       },
 
       data4: {
-        labels: ["1月", "2月", "3月"],
+                labels: [
+          "Eating",
+          "Drinking",
+          "Sleeping",
+          "Designing",
+          "Coding",
+          "Cycling",
+          "Running"
+        ],
         datasets: [
           {
-            label: "mm",
-            data: [80, 80, 80],
-            data2: [],
-            borderWidth: 1,
-            borderColor: "#FC2525",
-            pointBackgroundColor: "rgba(255,a 0,0, 0.5)",
-            pointBorderColor: "white",
-            backgroundColor: "rgba(255, 0,0, 0.5)"
+            label: "My First dataset",
+            backgroundColor: "rgba(179,181,198,0.2)",
+            borderColor: "rgba(179,181,198,1)",
+            pointBackgroundColor: "rgba(179,181,198,1)",
+            pointBorderColor: "#fff",
+            pointHoverBackgroundColor: "#fff",
+            pointHoverBorderColor: "rgba(179,181,198,1)",
+            data: [65, 59, 90, 81, 56, 55, 40]
+          },
+          {
+            label: "My Second dataset",
+            backgroundColor: "rgba(255,99,132,0.2)",
+            borderColor: "rgba(255,99,132,1)",
+            pointBackgroundColor: "rgba(255,99,132,1)",
+            pointBorderColor: "#fff",
+            pointHoverBackgroundColor: "#fff",
+            pointHoverBorderColor: "rgba(255,99,132,1)",
+            data: [28, 48, 40, 19, 96, 27, 100]
           }
         ]
       },
