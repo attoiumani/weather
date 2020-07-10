bc9115222f3d055ef01cc6f11ff37cd54d47a9b8
attoiumani <attoiumani0313@gmail.com>
2020-07-10 21:37:24 +0900

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,16 @@
-const functions = require('firebase-functions');
+const https = require('https');
 
-exports.helloWorld = functions.https.onRequest((req, res) => {
-    res.send("Hellotest");
+const req = https.request('https://randomuser.me/api/?inc=gender,name,nat&format=PrettyJSON', (res) => {
+    res.on('data', (chunk) => {
+        console.log(`BODY: ${chunk}`);
+    });
+    res.on('end', () => {
+        console.log('No more data in response.');
+    });
+})
+
+req.on('error', (e) => {
+  console.error(`problem with request: ${e.message}`);
 });
+
+req.end();
