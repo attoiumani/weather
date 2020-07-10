9a59c4605a83026a9938e89bf782ffe5229fc2d5
attoiumani <attoiumani0313@gmail.com>
2020-07-10 16:47:14 +0900

    Create firebase.js

---
+++ src/plugins/firebase.js
@@ -0,0 +1,18 @@
+
+
+import firebase from 'firebase'
+
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: "AIzaSyCz_q-qIm6TKBDi-w4Qf3cyFToKYb2-mT8",
+    authDomain: "sample-3463a.firebaseapp.com",
+    databaseURL: "https://sample-3463a.firebaseio.com",
+    projectId: "sample-3463a",
+    storageBucket: "sample-3463a.appspot.com",
+    messagingSenderId: "748303189274",
+    appId: "1:748303189274:web:78ec7c2d8ae358732558a3",
+    measurementId: "G-D5498K3MP4"
+  })
+}
+
+export default firebase
