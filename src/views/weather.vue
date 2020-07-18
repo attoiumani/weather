6e5442aeb51e606ee7952ca0cc576c380eae565d
attoiumani <attoiumani0313@gmail.com>
2020-07-18 13:53:42 +0900

    Update weather.vue

--- src/views/weather.vue
+++ src/views/weather.vue
@@ -1,34 +1,30 @@
 <template>
   <v-row>
-    <v-col cols="12" sm="12" md="6" lg="4" xl="4">
-      <weather place="sapporo" />
-    </v-col>
-    <v-col cols="12" sm="12" md="6" lg="4" xl="4">
-      <weather place="sendai" />
-    </v-col>
-    <v-col cols="12" sm="12" md="6" lg="4" xl="4">
-      <weather place="tokyo" />
-    </v-col>
-    <v-col cols="12" sm="12" md="6" lg="4" xl="4">
-      <weather place="kanazawa" />
-    </v-col>
-    <v-col cols="12" sm="12" md="6" lg="4" xl="4">
-      <weather place="osaka" />
-    </v-col>
-    <v-col cols="12" sm="12" md="6" lg="4" xl="4">
-      <weather place="fukuoka" />
+    <v-col :key="item.id" v-for="item in Items" cols="12" sm="12" md="6" lg="4" xl="4">
+      <weather :place="item.place" />
     </v-col>
   </v-row>
 </template>
 
 
 <script>
-
 import weather from "@/components/weather";
 
 export default {
   components: {
     weather
+  },
+  data() {
+    return {
+      Items: [
+        { id: 1, place: "sapporo" },
+        { id: 2, place: "sendai" },
+        { id: 3, place: "tokyo" },
+        { id: 4, place: "kanazawa" },
+        { id: 5, place: "osaka" },
+        { id: 6, place: "fukuoka" }
+      ]
+    };
   }
 };
 </script>
