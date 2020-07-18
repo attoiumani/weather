b9a513c959c78dcf2cee51de9b9d87cccfe6e09e
attoiumani <attoiumani0313@gmail.com>
2020-07-18 20:09:41 +0900

    Update weather.vue

--- src/views/weather.vue
+++ src/views/weather.vue
@@ -1,28 +1,8 @@
 <template>
   <v-row>
-  <v-container id="dropdown-example-3">
-    <v-overflow-btn
-      class="my-2"
-      :items="dropdown_edit"
-      label="Overflow Btn w/ counter"
-      counter
-      item-text="text"
-      tem-value="text"
-    ></v-overflow-btn>
-  </v-container>
-    <div>
-      <ul>
-        <li>
-          <button @click="sort(0)">0℃→1℃</button>
-        </li>
-        <li>
-          <button @click="sort(1)">A→Z</button>
-        </li>
-        <li>
-          <button @click="sort(2)">N→S</button>
-        </li>
-      </ul>
-    </div>
+    <v-container>
+      <v-overflow-btn :items="dropdown" label="select & click!" segmented></v-overflow-btn>
+    </v-container>
     <v-col :key="item.id" v-for="item in Items" cols="12" sm="12" md="6" lg="4" xl="4">
       <weather :place="item.place" />
     </v-col>
@@ -47,40 +27,28 @@ export default {
         { id: 5, place: "osaka" },
         { id: 6, place: "fukuoka" }
       ],
-      dropdown_edit: [
-        { text: "0℃→1℃" },
-        { text: "A→Z" },
-        { text: "N→S" },
+      dropdown: [
+        {
+          text: "0℃→1℃",
+          callback: () =>
+            this.Items.sort(function(a, b) {
+              if (a.id < b.id) return -1;
+              if (a.id > b.id) return 1;
+              return 0;
+            })
+        },
+        {
+          text: "A→Z",
+          callback: () =>
+            this.Items.sort(function(a, b) {
+              if (a.place < b.place) return -1;
+              if (a.place > b.place) return 1;
+              return 0;
+            })
+        },
+        { text: "N→S" }
       ]
     };
   },
-  methods: {
-    sort(index) {
-      switch (index) {
-        case 0:
-          this.Items.sort(function(a, b) {
-            if (a.id < b.id) return -1;
-            if (a.id > b.id) return 1;
-            return 0;
-          });
-          break;
-        case 1:
-          this.Items.sort(function(a, b) {
-            if (a.place < b.place) return -1;
-            if (a.place > b.place) return 1;
-            return 0;
-          });
-          break;
-        case 2:
-          this.sortItems.sort(function(a, b) {
-            if (a.age < b.age) return -1;
-            if (a.age > b.age) return 1;
-            return 0;
-          });
-          break;
-        default:
-      }
-    }
-  }
 };
 </script>
