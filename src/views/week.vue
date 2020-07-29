32a238d645dfd380d2bd9702d677eb530db8d921
attoiumani <attoiumani0313@gmail.com>
2020-07-29 23:59:08 +0900

    Update week.vue

--- src/views/week.vue
+++ src/views/week.vue
@@ -4,6 +4,9 @@
     {{date3}}
     <img v-bind:src="icon" />
     {{temp}}
+    <img v-bind:src="icon" />
+    {{ftemp}}
+    {{fdate}}
   </div>
 </template>
 
@@ -22,15 +25,20 @@ export default {
       date3: null,
       temp: null,
       icon: null,
+
+
+      ftemp:null,
+      fdt:null,
+      fdte:null
     };
   },
   created: function () {
     axios;
 
     let getUrl ="https://api.openweathermap.org/data/2.5/onecall?lat=35.681236&lon=139.767125&units=metric&";
+    let getUrl2 ="http://api.openweathermap.org/data/2.5/forecast?lat=35.681236&lon=139.767125&units=metric&lang=ja&";
     let getKey = "appid=4dff50a83aa2145ba555d8f59e9d3ef0";
-    let Url = getUrl + getKey;
-    return axios.get(Url).then(
+      axios.get(getUrl + getKey).then(
       function (response) {
         this.dt = response.data.current.dt;
         this.date = new Date(this.dt * 1000).toLocaleDateString("ja-JP").slice(5);
@@ -40,7 +48,19 @@ export default {
         this.icon ="https://openweathermap.org/img/w/" +response.data.current.weather[0].icon +".png";
       }.bind(this)
     );
+
+      axios.get(getUrl2 + getKey).then(
+      function (response) {
+        this.ftemp = response.data.list[0].main.temp;
+        this.fdt = response.data.list[0].dt_txt;
+        this.fdate=this.fdt.slice(11,16);
+      }.bind(this)
+    );
+
   },
   methods: {},
 };
+
+//https://api.openweathermap.org/data/2.5/onecall?lat=35.681236&lon=139.767125&units=metric&appid=4dff50a83aa2145ba555d8f59e9d3ef0
+//http://api.openweathermap.org/data/2.5/forecast?lat=35.681236&lon=139.767125&units=metric&lang=ja&APPID=4dff50a83aa2145ba555d8f59e9d3ef0
 </script>
