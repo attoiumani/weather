c483ac6d8a8eb30dd714accf26a00bc0ad3efd17
attoiumani <attoiumani0313@gmail.com>
2020-07-30 19:34:13 +0900

    Create test.vue

---
+++ src/views/test.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <GChart
+      :settings="{ packages: ['geochart']}"
+      type="GeoChart"
+      :data="chartData"
+      :options="chartOptions"
+    />
+  </div>
+</template>
+
+<script>
+import { GChart } from "vue-google-charts";
+export default {
+  name: "App",
+  components: {
+    GChart
+  },
+  data() {
+    return {
+      // Array will be automatically processed with visualization.arrayToDataTable function
+      chartData: [
+        ["States", "temp"],
+        ["北海道", 59406],
+        ["青森", 42103],
+        ["秋田", 40671],
+      ],
+      chartOptions: {
+        colorAxis: {
+          colors: ["f4e0d6", "ca6633"]
+        },
+        backgroundColor: "transparent",
+        datalessRegionColor: "white",
+        defaultColor: "white",
+        region: "JP",
+        displayMode: "region",
+        resolution: "provinces",
+        width: 500
+      }
+    };
+  },
+
+};
+</script>
