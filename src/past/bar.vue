b5bbac16f8f10556ffcad22b76d530dbd3643445
attoiumani <attoiumani0313@gmail.com>
2020-07-31 23:49:16 +0900

    Create bar.vue

---
+++ src/past/bar.vue
@@ -0,0 +1,21 @@
+<script>
+import { Bar } from "vue-chartjs";
+const MyChart = {
+  extends: Bar,
+  props: ["data", "options"],
+  mounted() {
+    this.renderChart(this.data , this.options);
+  },
+  /*watch: {
+    data: {
+      handler() {
+        this.renderChart(this.data, this.options);
+        // for more errors uncomment this instead:
+        // this.$data._chart.update();
+      },
+      deep: true
+    }
+  }*/
+};
+export default MyChart;
+</script>
