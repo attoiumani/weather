bcdf05f48e8233c027c8672c7399016a19369086
attoiumani <attoiumani0313@gamil.com>
2020-07-31 15:33:24 +0900

    Update test.vue

--- src/views/test.vue
+++ src/views/test.vue
@@ -15,11 +15,11 @@ prepend や append の slots を使用すれば、v-slider をどんな場所に
         <span class="subheading font-weight-light mr-1">BPM</span>
       </v-col>
     </v-row>
-    <v-slider v-model="bpm" :color="color" track-color="grey" always-dirty min="40" max="218"></v-slider>
+    <v-slider v-model="bpm" :color="color" track-color="grey" always-dirty min="40" :max="100"></v-slider>
 
     <v-row class="mb-4" justify="space-between">
       <v-col class="text-left">
-        <span class="display-3 font-weight-light" v-text="fruits"></span>
+        <span class="display-3 font-weight-light">{{fruits}}</span>
       </v-col>
     </v-row>
     <v-slider
@@ -37,15 +37,14 @@ prepend や append の slots を使用すれば、v-slider をどんな場所に
 
 <script>
 export default {
-  data: () => ({
+  data() {
+    return {
     slider: 45,
     ticksLabels: ["Figs", "Lemon", "Pear", "Apple", "test"],
     bpm: 40,
     fruits: 5,
-    interval: null,
-    isPlaying: false,
-  }),
+  }
+  },
 
-  methods: {},
 };
 </script>
