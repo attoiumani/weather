cc472b464e197687da0ce2534fa85fc64ce7f9e1
attoiumani <attoiumani0313@gmail.com>
2020-08-01 10:57:35 +0900

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
     <v-navigation-drawer v-model="drawer" expand-on-hover clipped app>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="title">Application</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
       <v-divider></v-divider>
       <v-list dense nav>
         <v-list-item v-for="item in items" :key="item.title" :to="item.path">
