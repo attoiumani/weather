98bc3a878a27de80376d90b3424b3e7791b7f3c9
attoiumani <attoiumani0313@gmail.com>
2020-08-01 11:38:53 +0900

    Update weather.vue

--- src/views/weather.vue
+++ src/views/weather.vue
@@ -80,6 +80,15 @@ export default {
               return 0;
             }),
         },
+        {
+          text: "1℃→0℃",
+          callback: () =>
+            this.Items.sort(function (a, b) {
+              if (a.temp > b.temp) return -1;
+              if (a.temp < b.temp) return 1;
+              return 0;
+            }),
+        },
       ],
 
       chartOptions: {
